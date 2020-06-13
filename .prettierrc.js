40558201097a19d0954dbf6760eafe65c52fd41b
Anggella Fortunato <anggella.fortunato@gmail.com>
2020-06-13 12:23:37 +0200

    Initial configuration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  trailingComma: 'es5',
+  semi: false,
+  arrowParens: 'avoid',
+  quoteProps: 'consistent',
+}
